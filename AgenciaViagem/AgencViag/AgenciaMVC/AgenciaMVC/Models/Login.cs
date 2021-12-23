6663a04c260e2c000cdd385c5fb11f1f65159ce2
ThaisdosSantos <96456496+ThaisdosSantos@users.noreply.github.com>
2021-12-23 03:12:48 -0300

    Delete AgenciaViagem directory

--- AgenciaViagem/AgencViag/AgenciaMVC/AgenciaMVC/Models/Login.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AgenciaMVC.Models
-{
-    public class Login
-    {
-        public int  CodLogin { get; set; }
-        public string Email { get; set; }
-        public string  Senha { get; set; }
-    }
-}
